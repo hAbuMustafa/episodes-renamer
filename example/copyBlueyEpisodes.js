28ea4754fba291d1555d8338a0038a253d3ad3a9
hAbuMustafa <hosam5553@gmail.com>
2026-07-05 19:37:14 +0300

    change episode map

--- example/copyBlueyEpisodes.js
+++ example/copyBlueyEpisodes.js
@@ -1,16 +1,16 @@
 // get episodes from https://en.wikipedia.org/wiki/List_of_Bluey_episodes
 
-function copyBlueyEpisodes(){
-	navigator.clipboard.writeText(
-		JSON.stringify(
-		    Array.from(document.querySelectorAll('tr:has(td:nth-of-type(6))')).map(ep=>(
-        {
-          season:Number(ep.querySelector('th').textContent),
-          episode:Number(ep.querySelector('td').textContent),
-          title:ep.querySelector('td:nth-of-type(2)').textContent.replaceAll("\"","")
-        }
-      )),
-			null, 4
-		)
-	)
+function copyBlueyEpisodes() {
+  const episodes = {};
+
+  document.querySelectorAll('tr:has(td:nth-of-type(6))').forEach((node) => {
+    const overallEpNum = Number(node.querySelector('th').textContent);
+    const s = String(Math.ceil(overallEpNum / 52)).padStart(2, '0');
+    const e = String(overallEpNum % 52).padStart(2, '0');
+
+    episodes[`S${s}E${e}`] = {
+      title: node.querySelector('td:nth-of-type(2)').textContent.replaceAll('"', ''),
+    };
+  });
+  navigator.clipboard.writeText(JSON.stringify(episodes, null, 4));
 }
